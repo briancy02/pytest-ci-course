85d6e8b33754c36dfbdd58f3c11797ea0136eb65
briancy02 <byoon2002@gmail.com>
2021-01-05 00:16:11 +0200

    minor tweaks to example_test

--- test_example.py
+++
@@ -1,5 +0,0 @@
-from example import is_even
-
-def test_is_even():
-  assert is_even(2) is True
-  assert is_even(3) is False
--- tests/example_test.py
+++ tests/example_test.py
@@ -1,2 +1,5 @@
-def example_test() -> None:
-    assert False
+from example import is_even
+
+def test_is_even():
+  assert is_even(2) is True
+  assert is_even(3) is False
