5c0b19ec040050777484bedd0c8e3e37ca9ed3a8
adamwu2 <adamwu2@illinois.edu>
2021-01-04 20:38:52 -0600

    add files back

---
+++ example.py
@@ -0,0 +1,6 @@
+def is_even(x):
+  # Insert your code here
+  if x % 2 == 0:
+    return True
+  else:
+    return False
---
+++ example_test.py
@@ -0,0 +1,5 @@
+from example_pkg import example
+
+def test_is_even():
+  assert example.is_even(2) is True
+  assert example.is_even(3) is False
